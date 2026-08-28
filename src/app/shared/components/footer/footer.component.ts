19347bd549f6b482e192bfa60a4f9a8cbd34a5e2
Victor Arruda <victor.arruda.magalhaes@gmail.com>
2026-08-28 10:39:55 -0300

    feat: footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
     selector: 'app-footer',
     standalone: true,
     styleUrl: './footer.component.scss',
     templateUrl: './footer.component.html',
+    imports: [ButtonComponent],
 })
 export class FooterComponent {}
