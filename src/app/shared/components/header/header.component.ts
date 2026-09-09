723f36d99d552a83777b1989478799a01517d2fd
Victor Arruda <victor.arruda.magalhaes@gmail.com>
2026-09-09 12:21:18 -0300

    feat: update navbar with toggle option

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -13,12 +13,26 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 })
 export class HeaderComponent {
     protected readonly menuOpen = signal(false);
+    protected readonly participationOpen = signal(false);
+    protected readonly solutionsOpen = signal(false);
 
     protected toggleMenu() {
         this.menuOpen.update((isOpen) => !isOpen);
     }
 
+    protected toggleSolutions() {
+        this.participationOpen.set(false);
+        this.solutionsOpen.update((isOpen) => !isOpen);
+    }
+
+    protected toggleParticipation() {
+        this.solutionsOpen.set(false);
+        this.participationOpen.update((isOpen) => !isOpen);
+    }
+
     protected closeMenu() {
         this.menuOpen.set(false);
+        this.participationOpen.set(false);
+        this.solutionsOpen.set(false);
     }
 }
