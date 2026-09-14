b3a72e24adbc4b51aeebfcaf9dffd06cb8c82fed
Victor Arruda <victor.arruda.magalhaes@gmail.com>
2026-09-14 14:49:00 -0300

    feat: milestone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,29 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    loadComponent: () => import('./layouts/main/main.layout').then((m) => m.MainLayout),
+    children: [
+      {
         path: '',
-        loadComponent: () => import('./layouts/main/main.layout').then(m => m.MainLayout),
-        children: [
-            {
-                path: '',
-                loadComponent: () => import('./features/home/home.page').then(m => m.HomePage)
-            },
-            {
-                path: 'solucoes-para-criadores',
-                loadComponent: () => import('./features/para-criadores/para-criadores.page').then(m => m.ForCreatorsPage)
-            },
-            {
-                path: 'solucoes-para-marcas',
-                loadComponent: () => import('./features/para-marcas/para-marcas.page').then(m => m.ForBrandsPage)
-            },
-        ],
-    },
+        loadComponent: () => import('./features/home/home.page').then((m) => m.HomePage),
+      },
+      {
+        path: 'solucoes-para-criadores',
+        loadComponent: () =>
+          import('./features/para-criadores/para-criadores.page').then((m) => m.ForCreatorsPage),
+      },
+      {
+        path: 'solucoes-para-marcas',
+        loadComponent: () =>
+          import('./features/para-marcas/para-marcas.page').then((m) => m.ForBrandsPage),
+      },
+      {
+        path: 'eventos',
+        loadComponent: () =>
+          import('./features/eventos/eventos.page').then((m) => m.EventsPage),
+      },
+    ],
+  },
 ];
